c27b4553a6703e83782c61392e56d995ca9d8447
Nazarii <dzhaman.nazar2003@gmail.com>
2024-08-22 11:12:47 +0300

    add courses

--- src/routes/courses.ts
+++ src/routes/courses.ts
@@ -1,9 +1,11 @@
 import { Request, Response, Router } from "express";
+import { Course } from "../models/course.js";
 
 const coursesRoutes = Router();
 
-coursesRoutes.get("/", (req: Request, res: Response): void => {
-	res.render("courses", { title: "Courses page", isCourses: true });
+coursesRoutes.get("/", async (req: Request, res: Response): Promise<void> => {
+	const courses = await Course.getAll();
+	res.render("courses", { title: "Courses page", isCourses: true, courses });
 });
 
 export { coursesRoutes };
