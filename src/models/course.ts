56e1d94a7a28ac33b8a8353e7fedc729b8a4d191
Nazarii <dzhaman.nazar2003@gmail.com>
2024-08-22 16:41:32 +0300

    add dinamic params and edit course

--- src/models/course.ts
+++ src/models/course.ts
@@ -15,16 +15,24 @@ class Course implements ICourse {
 		this.id = uuid();
 	}
 
-    async save(): Promise<void> {
-        try {
-            const courses = await Course.getAll();
-            const newCourse = { title: this.title, price: this.price, img: this.img, id: this.id };
-            courses.push(newCourse);
-            await fs.writeFile("src/data/courses.json", JSON.stringify(courses, null, 2));
-        } catch (error) {
-            console.error("Error saving courses:", error);
-        }
-    }
+	async save(): Promise<void> {
+		try {
+			const courses = await Course.getAll();
+			const newCourse = {
+				title: this.title,
+				price: this.price,
+				img: this.img,
+				id: this.id,
+			};
+			courses.push(newCourse);
+			await fs.writeFile(
+				"src/data/courses.json",
+				JSON.stringify(courses, null, 2)
+			);
+		} catch (error) {
+			console.error("Error saving courses:", error);
+		}
+	}
 
 	static async getAll(): Promise<ICourse[]> {
 		try {
@@ -35,6 +43,40 @@ class Course implements ICourse {
 			throw error;
 		}
 	}
+
+	static async getById(id: string): Promise<ICourse> {
+		try {
+			const courses = await Course.getAll();
+			const course = courses.find((c) => c.id === id);
+			if (!course) {
+				throw new Error(`Course with id ${id} not found`);
+			}
+			return course;
+		} catch (error) {
+			console.error("Error reading or parsing the file:", error);
+			throw error;
+		}
+	}
+
+
+	static async updateById(course: ICourse): Promise<void> {
+		try {
+			const courses = await Course.getAll();
+			const idx = courses.findIndex((c) => c.id === course.id);
+			if (idx === -1) {
+				throw new Error(`Course with id ${course.id} not found`);
+			}
+			courses[idx] = course;
+			await fs.writeFile(
+				"src/data/courses.json",
+				JSON.stringify(courses, null, 2)
+			);
+		} catch (error) {
+			console.error("Error updating the course:", error);
+			throw error;
+		}
+	}
+
 }
 
 export { Course };
--- src/routes/courses.ts
+++ src/routes/courses.ts
@@ -8,4 +8,31 @@ coursesRoutes.get("/", async (req: Request, res: Response): Promise<void> => {
 	res.render("courses", { title: "Courses page", isCourses: true, courses });
 });
 
+coursesRoutes.get(
+	"/:id",
+	async (req: Request, res: Response): Promise<void> => {
+		const course = await Course.getById(req.params.id);
+		res.render("course", {layout: "empty", title: `Course ${course.title}`, course });
+	}
+);
+
+coursesRoutes.get(
+	"/:id/edit",
+	async (req: Request, res: Response): Promise<void> => {
+		if(!req.query.allow) {
+			return res.redirect("/");
+		}	
+		const course = await Course.getById(req.params.id);
+		res.render("course-edit", {title: `Edit ${course.title}`, course})
+	}
+);
+
+coursesRoutes.post(
+	"/edit",
+	async (req: Request, res: Response): Promise<void> => {
+		await Course.updateById(req.body);
+		res.redirect("/courses");
+	}
+);
+
 export { coursesRoutes };
