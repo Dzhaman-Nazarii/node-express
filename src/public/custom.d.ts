debc232b445a6dbd2f35affcc8a656bc8f60f0eb
Nazarii <dzhaman.nazar2003@gmail.com>
2024-08-28 11:02:26 +0300

    add session

--- src/public/custom.d.ts
+++ src/public/custom.d.ts
@@ -1,9 +1,22 @@
 import { IUser } from "../models/user.interface";
+import session from "express-session";
 
 declare global {
   namespace Express {
     interface Request {
       user?: IUser;
     }
+  }
+}
+
+declare module "express-session" {
+  interface SessionData {
+    isAuthenticated?: boolean;
+  }
+}
+
+declare module "express-session" {
+  interface SessionData {
+    user?: User;
   }
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,5 @@
 import { Request, Response, Router } from "express";
+import { User } from "../models/user.js";
 
 const authRoutes = Router();
 
@@ -6,7 +7,25 @@ authRoutes.get("/login", async (req: Request, res: Response) => {
 	res.render("auth/login", {
 		title: "Authorization",
 		isLogin: true,
-	})
-})
+	});
+});
 
-export {authRoutes};
+authRoutes.post("/login", async (req: Request, res: Response) => {
+	const user = await User.findById("66cca40cbe84546dc77444b4").exec();
+	req.session.user = user;
+	req.session.isAuthenticated = true;
+	req.session.save((error) => {
+		if (error) {
+			throw error;
+		}
+		res.redirect("/");
+	});
+});
+
+authRoutes.get("/logout", async (req: Request, res: Response) => {
+	req.session.destroy(() => {
+		res.redirect("/auth/login#login");
+	});
+});
+
+export { authRoutes };
