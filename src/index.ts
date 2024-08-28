8ad8edd6130db7c1ba322dfe97c12d7ed89c48d7
Nazarii <dzhaman.nazar2003@gmail.com>
2024-08-28 11:16:36 +0300

    add session in database

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { engine } from "express-handlebars";
 import session from "express-session";
+import connectMongoDBSession from 'connect-mongodb-session';
 import variablesMiddleware from './middleware/variables.js';
 import Handlebars from "handlebars";
 import { allowInsecurePrototypeAccess } from "@handlebars/allow-prototype-access";
@@ -13,6 +14,7 @@ import { cardRoutes } from "./routes/card.js";
 import { ordersRoutes } from "./routes/orders.js";
 import { authRoutes } from "./routes/auth.js";
 
+const MONGODB_URI = "mongodb+srv://Nazarii:DkYgW.dCdt2AyH3@cluster0.nxh4bxc.mongodb.net/Shop";
 const app = express();
 
 dotenv.config();
@@ -25,20 +27,14 @@ app.engine(
 	})
 );
 
+const MongoDBStore = connectMongoDBSession(session);
+const store = new MongoDBStore({
+	uri: MONGODB_URI,
+	collection: 'sessions',
+  });
+
 app.set("view engine", "handlebars");
 app.set("views", "./src/views");
-
-// app.use(async (req: Request, res: Response, next: NextFunction) => {
-// 	try {
-// 		const user = await User.findById("66cca40cbe84546dc77444b4").exec();
-// 		req.user = user as IUser | undefined;
-// 		next();
-// 	} catch (error) {
-// 		console.error(error);
-// 		res.status(500).send("Internal Server Error");
-// 	}
-// });
-
 app.use(express.static("./dist/public"));
 app.use(express.static("./src/public"));
 app.use(express.urlencoded({ extended: true }));
@@ -47,6 +43,7 @@ app.use(
 		secret: "some secret value",
 		resave: false,
 		saveUninitialized: false,
+		store
 	})
 );
 app.use(variablesMiddleware);
@@ -59,18 +56,7 @@ app.use("/auth", authRoutes);
 
 async function start() {
 	try {
-		const url =
-			"mongodb+srv://Nazarii:DkYgW.dCdt2AyH3@cluster0.nxh4bxc.mongodb.net/Shop";
-		await mongoose.connect(url);
-		// const candidate = await User.findOne();
-		// if (!candidate) {
-		// 	const user = new User({
-		// 		email: "nazar@gmail.com",
-		// 		name: "Nazarii",
-		// 		card: { items: [] },
-		// 	});
-		// 	await user.save();
-		// }
+		await mongoose.connect(MONGODB_URI);
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
 		});
--- src/public/custom.d.ts
+++ src/public/custom.d.ts
@@ -20,3 +20,22 @@ declare module "express-session" {
     user?: User;
   }
 }
+
+declare module 'connect-mongodb-session' {
+  import { Store } from 'express-session';
+
+  interface MongoDBStoreOptions {
+    uri: string;
+    collection: string;
+    connectionOptions?: any;
+    expires?: number;
+    idField?: string;
+    databaseName?: string;
+  }
+
+  function connectMongoDBSession(session: any): {
+    new (options: MongoDBStoreOptions): Store;
+  };
+
+  export = connectMongoDBSession;
+}
